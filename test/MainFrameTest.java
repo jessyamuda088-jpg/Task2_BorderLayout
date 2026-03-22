7423a25f1f37fef5cb628a9b8b735992be81d454
Jessica Amuda <jessyamuda088@gmail.com>
2026-03-23 00:18:57 +0300

    Add unit tests for MainFrame functionality

--- test/MainFrameTest.java
+++ test/MainFrameTest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import javax.swing.JButton;
 
 public class MainFrameTest {
+
+    @Test
+    public void testAdditionner() {
+        MainFrame frame = new MainFrame();
+        int resultat = frame.additionner(2, 3);
+        assertEquals(5, resultat);
+    }
+
+    @Test
+    public void testSubmitButtonExists() {
+        MainFrame frame = new MainFrame();
+        
+        java.awt.Component[] components = frame.getContentPane().getComponents();
+        boolean found = false;
+        
+        for (java.awt.Component comp : components) {
+            if (comp instanceof JButton) {
+                JButton button = (JButton) comp;
+                if ("Submit".equals(button.getText())) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        
+        assertTrue("Submit button not found", found);
+    }
 }
